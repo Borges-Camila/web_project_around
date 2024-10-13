1b64d8351b6ece5760c6dc8c6989c11f1424e895
Camila Borges <camis02121999@gmail.com>
2024-10-13 10:50:09 -0300

    Modificações em CSS para melhor estilizar a página, estrutura básica javascript para fazer o popup abrir e fechar

---
+++ index.js
@@ -0,0 +1,33 @@
+const editButton = document.querySelector(".profile__edit-button")
+const popup = document.querySelector(".popup")
+const closeButton = document.querySelector(".popup__close-button")
+
+const indexName = document.querySelector("#input-name")
+const indexAbout = document.querySelector("#input-about")
+
+const profileName = document.querySelector(".profile__title")
+const profileAbout = document.querySelector(".profile__subtitle-text")
+
+const saveButton = document.querySelector(".popup__save-button")
+
+
+function openPopup() {
+  popup.classList.add("popup__change")
+}
+editButton.addEventListener("click", openPopup)
+
+function closePopup() {
+  popup.classList.remove("popup__change")
+}
+closeButton.addEventListener("click", closePopup)
+
+function updateProfileInfo(evento) {
+  evento.preventDefault()
+  if(indexName.value != "" && indexAbout.value != "") {
+    profileName.textContent = indexName.value
+    profileAbout.textContent = indexAbout.value
+    closePopup()
+  }
+}
+
+saveButton.addEventListener("click", updateProfileInfo)
